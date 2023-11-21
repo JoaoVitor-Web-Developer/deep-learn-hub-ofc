fe08bb2aebcb6a54d3073340c1ff4da97a237c33
João Vitor Targino De Oliveira <119077083+JoaoVitor-Web-Developer@users.noreply.github.com>
2023-11-21 09:39:17 -0300

    Update route.ts

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -1,70 +1,61 @@
-// pages/api/stripe.ts
-
 import { getAuthSession } from "@/lib/auth";
 import { prisma } from "@/lib/db";
 import { stripe } from "@/lib/stripe";
-import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
 const settingsUrl = process.env.NEXTAUTH_URL + "/settings";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  try {
-    const session = await getAuthSession(req);
+export async function GET() {
+ try {
+   const session = await getAuthSession();
+   if (!session?.user) {
+     return new NextResponse("Sem autorização", { status: 401 });
+   }
 
-    if (!session?.user) {
-      return new NextResponse("Sem autorização", { status: 401 });
-    }
+   const userSubscription = await prisma.userSubscription.findUnique({
+     where: {
+       userId: session.user.id,
+     },
+   });
 
-    const userSubscription = await prisma.userSubscription.findUnique({
-      where: {
-        userId: session.user.id,
-      },
-    });
+   if (userSubscription && userSubscription.stripeCustomerId) {
+     const stripeSession = await stripe.billingPortal.sessions.create({
+       customer: userSubscription.stripeCustomerId,
+       return_url: settingsUrl,
+     });
+     return new NextResponse(JSON.stringify({ url: stripeSession.url }), { status: 200, headers: { 'Content-Type': 'application/json' } });
+   }
 
-    let stripeSession;
-
-    if (userSubscription && userSubscription.stripeCustomerId) {
-      stripeSession = await stripe.billingPortal.sessions.create({
-        customer: userSubscription.stripeCustomerId,
-        return_url: settingsUrl,
-      });
-    } else {
-      stripeSession = await stripe.checkout.sessions.create({
-        success_url: settingsUrl,
-        cancel_url: settingsUrl,
-        payment_method_types: ["card"],
-        mode: "subscription",
-        billing_address_collection: "auto",
-        customer_email: session.user.email ?? "",
-        line_items: [
-          {
-            price_data: {
-              currency: "BRL",
-              product_data: {
-                name: "Deep Learn Hub",
-                description: "Geração de cursos ilimitados!",
-              },
-              unit_amount: 990,
-              recurring: {
-                interval: "month",
-              },
-            },
-            quantity: 1,
-          },
-        ],
-        metadata: {
-          userId: session.user.id,
-        },
-      });
-    }
-
-    return res.json({ url: stripeSession.url });
-  } catch (error) {
-    console.error("[STRIPE ERROR]", error);
-    return new NextResponse("internal server error", { status: 500 });
-  }
+   const stripeSession = await stripe.checkout.sessions.create({
+     success_url: settingsUrl,
+     cancel_url: settingsUrl,
+     payment_method_types: ["card"],
+     mode: "subscription",
+     billing_address_collection: "auto",
+     customer_email: session.user.email ?? "",
+     line_items: [
+       {
+         price_data: {
+           currency: "BRL",
+           product_data: {
+             name: "Deep Learn Hub",
+             description: "Geração de cursos ilimitados!",
+           },
+           unit_amount: 990,
+           recurring: {
+             interval: "month",
+           },
+         },
+         quantity: 1,
+       },
+     ],
+     metadata: {
+       userId: session.user.id,
+     },
+   });
+   return new NextResponse(JSON.stringify({ url: stripeSession.url }), { status: 200, headers: { 'Content-Type': 'application/json' } });
+ } catch (error) {
+   console.log("[STRIPE ERROR]", error);
+   return new NextResponse("internal server error", { status: 500 });
+ }
 }
